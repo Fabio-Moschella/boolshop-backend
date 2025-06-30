597cedf0f608fb9d520be413c3c3846a671039a1
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-06-30 17:42:12 +0200

    testing email

---
+++ services/emailService.js
@@ -0,0 +1,80 @@
+//! modulo riutilizzabile che contiene la logica per l'invio delle email
+
+//* Importo la libreria Nodemailer 
+const nodemailer = require('nodemailer');
+
+//* configuro il TRANSPORTER di Nodemailer importando le variabili dal file .env
+// TRANSPORTER = oggetto responsabile di COME e DA DOVE le email verranno inviate
+const transporter = nodemailer.createTransport({
+
+    // host: indirizzo del server SMTP del provider email
+    // "punto di partenza" da cui verranno spedite le email
+    host: process.env.SMTP_HOST,
+
+    // 'port': La porta che il server SMTP usa per la comunicazione.
+    // Tipicamente 587 (con STARTTLS) o 465 (con SSL/TLS).
+    port: process.env.SMTP_PORT,
+
+    // 'secure': Indica se la connessione deve usare connessione crittografata tramite SSL/TLS (Secure Sockets Layer / Transport Layer Security).
+    // Imposta a 'true' se la porta è 465 (SSL), a 'false' se è 587 (STARTTLS).
+    secure: process.env.SMTP_PORT == 465 ? true : false,
+
+    // 'auth': Credenziali per autenticarsi sul server SMTP.
+    auth: {
+        // user: email completa
+        user: process.env.EMAIL_USER,
+        // pass: password email
+        pass: process.env.EMAIL_PASS,
+    },
+
+    // 'tls': opzioni aggiuntive per la sicurezza della connessione TLS
+    // tls: {
+    //     // 'rejectUnauthorized: false': Disabilita la verifica dei certificati autofirmati.
+    //     // **IMPORTANTE**: Usare 'false' solo in AMBIENTE DI SVILUPPO. 
+    //     // In produzione, dovresti avere certificati validi e impostare a 'true' (o ometterlo).
+    //     rejectUnauthorized: false
+    // }
+});
+
+//* definisco la funzione 'sendEmail' che prende i dettagli dell'email e la invia
+/**
+ * @param {string} to - L'indirizzo email del destinatario.
+ * @param {string} subject - L'oggetto dell'email.
+ * @param {string} text - Il contenuto testuale semplice dell'email.
+ * @param {string} html - Il contenuto HTML dell'email (opzionale).
+ * @param {function} callback - Una funzione di callback che viene richiamata al termine dell'invio.
+ * Prende due argomenti: 'error' (se c'è un errore) e 'info' (informazioni sull'invio riuscito).
+ */
+const sendEmail = (to, subject, text, html, callback) => {
+    // preparazione oggetto con le opzioni dell'email
+    const mailOptions = {
+        from: `"NOME SITO" <${process.env.EMAIL_USER}>`,
+        // indirizzi destinatari
+        to: {},
+        // 'subject': L'oggetto dell'email.
+        subject: 'OGGETTO EMAIL',
+        // 'text': Il contenuto dell'email in formato testo semplice.
+        text: 'TESTO EMAIL',
+        // 'html': Il contenuto dell'email in formato HTML.
+        // Se fornito, prevale sul 'text' per i client email che supportano HTML.
+        // L'operatore '|| text' garantisce che ci sia sempre un contenuto, usando il 'text' se l'HTML non è fornito.
+        html: html || text
+    };
+
+    //* invio l'email usando il transporter
+    // 'transporter.sendMail()' è una funzione asincrona.
+    // Quando l'invio è completato (o fallisce), esegue la 'callback' fornita.
+    transporter.sendMail(mailOptions, (error, info) => {
+        //* gestisco il risultato nella callback
+        if (error) {
+            //se l'errore durante l'invio esiste, lo logga e lo passa alla callback
+            console.error('Errore durante l\'invio dell\'email:', error);
+            //passo l'errore alla callback (funzione passata come argomento)
+            return callback(error, null); 
+        }
+
+        // se l'invio è riuscito, loggo le info e le passo alla callback
+        console.log('Messaggio inviato: %s', info.messageId);
+        //
+    })
+}
