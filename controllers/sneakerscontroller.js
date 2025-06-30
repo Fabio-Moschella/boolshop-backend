088a014ca3a1c4ba598ca567bc0d4b5afda6f753
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-06-30 15:59:07 +0200

    fix query show

--- controllers/sneakerscontroller.js
+++ controllers/sneakerscontroller.js
@@ -25,6 +25,9 @@ const show = (req, res) => {
     if (err) return res.status(500).json({ error: "Database query failed" });
     if (results.length === 0)
       return res.status(404).json({ error: "sneaker not found" });
+    res.json({
+      results,
+    });
   });
 };
 
